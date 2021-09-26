e49a0ddb8813832e3be502590f5f02591ac2b39e
Kori Ryter <kryter@gmail.com>
2021-09-26 17:42:50 -0500

    Make the KeyboardInstrument use static methods.

--- src/instruments/KeyboardInstrument.ts
+++ src/instruments/KeyboardInstrument.ts
@@ -1,18 +1,7 @@
 import { Mechanics } from "../flying/Mechanics";
 
 export class KeyboardInstrument {
-  private static instance: KeyboardInstrument = null;
-
-  public static use() {
-    if (!KeyboardInstrument.instance) {
-      KeyboardInstrument.instance = new KeyboardInstrument();
-    }
-    return KeyboardInstrument.instance;
-  }
-
-  private constructor() { }
-
-  public pressEnter(): void {
+  public static pressEnter(): void {
     Mechanics.Keyboard.pressEnter();
   }
 }
