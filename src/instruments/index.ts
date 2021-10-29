fa13066754d707675678af14a7a4b60e623989d7
Kori Ryter <kryter@gmail.com>
2021-10-29 10:50:31 -0500

    Add an index for accessing instruments to make them easier to use in Page files.

---
+++ src/instruments/index.ts
@@ -0,0 +1,8 @@
+export { CheckboxInstrument } from './checkbox/CheckboxInstrument';
+export { ElementInstrument } from './element/ElementInstrument';
+export { KeyboardInstrument } from './keyboard/KeyboardInstrument';
+export { ListInstrument } from './list/ListInstrument';
+export { TextAreaInstrument } from './textArea/TextAreaInstrument';
+export { TextBoxInstrument } from './textBox/TextBoxInstrument';
+export { UrlInstrument } from './url/UrlInstrument';
+export { ButtonInstrument } from './button/ButtonInstrument';
