6049d815678b096914e7c2756e76b9c2fb295b34
Kori Ryter <kryter@gmail.com>
2022-01-09 12:17:25 -0600

    Add optional notes field for recording feature or bug ids that are related or for any other useful information.

--- src/FlightPlan.ts
+++ src/FlightPlan.ts
@@ -1,5 +1,13 @@
 import { FlightPlanLeg } from './FlightPlanLeg';
 
 export interface FlightPlan {
+  /**
+   * List any feature or bug ticket numbers that related to this
+   * flight plan.
+   * This can be useful for context while debugging failures and to
+   * help communicate what code changes are covered by tests.
+   */
+  notes?: string[];
+
   legs: FlightPlanLeg[];
 }
